22aa3cad7cc332503c3229da6936faf909322e65
Myriam <myriiamwayne@gmail.com>
2025-03-26 13:17:35 +0100

    Change const to let for task and task2 variables in taskBlock function

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+    let task = false;
+    let task2 = true;
   
     if (trueOrFalse) {
       let task = true;
