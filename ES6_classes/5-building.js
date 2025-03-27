6b5a0a49f97d93a6205aa710a29d528b1c0e8501
Myriam <myriiamwayne@gmail.com>
2025-03-27 23:03:15 +0100

    Enforce overriding of evacuationWarningMessage in Building subclasses

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,11 +4,11 @@ export default class Building {
         if (typeof sqft !== 'number') {
             throw new TypeError('Sqft must be a number');
         }
-        if (new.target === Building) {
-            throw new Error('Building is an abstract class and cannot be instantiated directly');
-        }
         this._sqft = sqft;
 
+        if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
     get sqft() {
         return this._sqft;
