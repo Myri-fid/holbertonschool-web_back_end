a4df5924372a63d5ce3dc2b105f283458b7e083f
Myriam <myriiamwayne@gmail.com>
2025-03-27 22:51:18 +0100

    Add Building class with validation and update main file to demonstrate usage

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default class Building {
+    constructor(sqft) {
+        if (typeof sqft !== 'number') {
+            throw new TypeError('Sqft must be a number');
+        }
+        this._sqft = sqft;
+    }
+    get sqft() {
+        return this._sqft;
+    }
+    evacuationWarningMessage() {
+        throw new TypeError('Class extending Building must override evacuationWarningMessage');
+    }
+}
