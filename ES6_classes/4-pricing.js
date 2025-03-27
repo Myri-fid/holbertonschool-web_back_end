d9f8e74d34300c003a9588eda505b0d0632e967b
Myriam <myriiamwayne@gmail.com>
2025-03-27 22:31:10 +0100

    Rename convert method to convertPrice for clarity

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -34,7 +34,7 @@ export default class Pricing {
     displayFullPrice() {
         return `${this._amount} ${this._currency.name} (${this._currency.code})`;
     }
-    static convert(amount, conversionRate) {
+    static convertPrice(amount, conversionRate) {
             return amount * conversionRate;
     }
 }
