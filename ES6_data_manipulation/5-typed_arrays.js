0beecf965661adbe70346bb394ea02ebe1d34bad
Myriam <myriiamwayne@gmail.com>
2025-04-01 13:09:36 +0200

    Add value assignment to Int8Array in createInt8TypedArray function

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,4 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
     }
     const buffer = new ArrayBuffer( length);
     var view = new Int8Array(buffer);
+    view[position] = value
+    return view;
+
 }
