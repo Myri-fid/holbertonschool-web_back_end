84794664640af99fcb9f36bb201cf8149b7db27e
Myriam <myriiamwayne@gmail.com>
2025-04-02 17:34:36 +0200

    Refactor main.js to use handleProfileSignup and remove loadBalancer example

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/ 
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import {signUpUser} from './4-user-promise';
+import {uploadPhoto} from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
