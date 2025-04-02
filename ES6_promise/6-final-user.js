145eba1038b9e4a1df3e41a2182b5b934cff9e0a
Myriam <myriiamwayne@gmail.com>
2025-04-02 17:35:26 +0200

    Refactor handleProfileSignup to improve result mapping and ensure consistent return structure

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,17 @@
 /*eslint-disable*/ 
-import {signUpUser} from './4-user-promise';
-import {uploadPhoto} from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    return {
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    };
-  }));
+  ]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: result.status, value: result.value };
+      }
+      return { status: result.status, value: result.reason };
+    });
+  });
 }
