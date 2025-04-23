c990b2be31194baa909f26357a65a86ab5286e6b
Myriam <myriiamwayne@gmail.com>
2025-04-23 22:09:28 +0200

    Update sum_list function to accept a list of floats as input

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ function sum_list which takes a list
 input_list of floats as argument"""
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ function sum_list which takes a list
     input_list of floats as argument"""
 
