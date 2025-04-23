6c3d10d9fabe2f1e19e57ec4481d20e4a78b6fc2
Myriam <myriiamwayne@gmail.com>
2025-04-23 22:07:39 +0200

    Implement sum_list function and update main.py for testing with float inputs

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function sum_list which takes a list
+input_list of floats as argument"""
+
+
+def sum_list(input_list: float) -> float:
+    """ function sum_list which takes a list
+    input_list of floats as argument"""
+
+    return sum(input_list)
