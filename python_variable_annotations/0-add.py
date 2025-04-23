3bee644c94e7a3cd86760d8c083d5daad9dddc85
Myriam <myriiamwayne@gmail.com>
2025-04-23 14:38:53 +0200

    Update docstrings in add.py for consistency and clarity

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""add that takes a float a and a float b as
+arguments and returns their sum"""
+
+
 def add(a: float, b: float) -> float:
+    """add that takes a float a and a float b as
+arguments and returns their sum"""
+
     return a + b
