ef6c335217761e0ae672b337c3c018d296358963
Myriam <myriiamwayne@gmail.com>
2025-04-23 20:19:33 +0200

    Implement floor function and update main.py for testing

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+"""floor which takes a float n as argument and
+returns the floor of float"""
+
+
+def floor(n: float) -> int:
+    """floor which takes a float n as argument and
+    returns the floor of float"""
+
+    floored = math.floor(n)
+    return floored
