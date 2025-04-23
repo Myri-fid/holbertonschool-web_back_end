36aecae0467ff2928f900f25f9911d2d6a45f2d1
Myriam <myriiamwayne@gmail.com>
2025-04-23 20:08:51 +0200

    Implement concat function and update main.py for testing

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function concat that takes a string str1 and a string str2"""
+
+    return str1 + str2
