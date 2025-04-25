d03bb303dfbbd545c3248450265dfb9ad79cbbb5
Myriam <myriiamwayne@gmail.com>
2025-04-25 14:42:21 +0200

    Implement wait_n coroutine and update main.py for testing

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""""""
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """"""
+    delays = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return sorted(delays)
