67672425bba302b4395d9c357a9cd9bf6bf0b7a5
Myriam <myriiamwayne@gmail.com>
2025-04-25 15:09:35 +0200

    Refactor wait_random coroutine and update wait_n to use it for task creation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-""". The list of the delays should be in ascending order without using sort()"""
+""". The list of the delays should be in
+ascending order without using sort()"""
 import asyncio
 from typing import List
 import random
-wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
-    """. The list of the delays should be in ascending order without using sort()"""
+async def wait_random(max_delay: int = 10) -> float:
+    """asynchronous coroutine that takes in an integer argument"""
 
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """. The list of the delays should be
+    in ascending order without using sort()"""
+
+    tasks = [wait_random(max_delay) for i in range(n)]
     delays = []
-    for completed in asyncio.as_completed(tasks):
-        result = await completed
+    for task in asyncio.as_completed(tasks):
+        result = await task
         delays.append(result)
     return delays
