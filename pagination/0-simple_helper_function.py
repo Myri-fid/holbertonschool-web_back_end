e14041b30f5e1a20f358943b8145a0907d46dc2e
Myriam <myriiamwayne@gmail.com>
2025-04-28 20:37:05 +0200

    Add index_range function and main file for pagination implementation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
-""""""
+""" return a tuple of size two containing a start index and an end index"""
 
 
+def index_range(page: int, page_size: int):
+    """ return a tuple of size two containing a start index and an end index"""
+
+    indexSize = page * page_size
+    indexStart = indexSize - page_size
+    indexEnd = indexStart + page_size
+    return indexStart, indexEnd
