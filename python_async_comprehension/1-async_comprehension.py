24aea69c506f76c23f186b3588e1e07b2af2bbfe
Myriam <myriiamwayne@gmail.com>
2025-04-28 18:10:21 +0200

    Refactor async_generator loop variable for clarity

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""coroutine called async_generator that takes no arguments."""
+
 from typing import AsyncGenerator
 import asyncio
 import random
@@ -11,3 +11,7 @@ async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
+async def async_comprehension() -> AsyncGenerator[float, None]:
+    """coroutine will collect 10 random numbers"""
+
