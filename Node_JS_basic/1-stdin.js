1d35c5b27045e9b4a80053fd37e4f6d1f3fb842f
Myriam <myriiamwayne@gmail.com>
2025-05-23 20:47:41 +0200

    Refactor stdin handling to improve data processing and code structure

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
 console.log("Welcome to Holberton School, what is your name?");
 
+process.stdin.setEncoding("utf8");
+
 process.stdin.on("data", (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  console.log(`Your name is: ${data.trim()}`);
+});
+
+process.stdin.on("end", () => {
   console.log("This important software is now closing");
-  process.exit();
 });
